63a68f50fe6394d039b763769caf23842852caf6
NaveedMohammed <rmdnaveed@gmail.com>
2025-04-13 11:37:01 -0500

    updated configurations

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    allowedHosts: ["localhost", "c2-stem.org", ".c2stem.org", "127.0.0.1"],
+  },
 });
